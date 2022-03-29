a3364bbd1e932ab1a4372e28211fc3194992a969
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-03-29 17:47:45 +0000

    Revert "Connection&service"

--- src/routes/authentificationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-router = express.Router();
-
-// require controller user
-const { Users } = require("../controllers");
-
-router.route("/register").post(Users.handleRegister);
