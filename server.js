f6a1abd86a3fae143b25156e828dc75be25a1d64
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-03-29 15:45:18 +0000

    CreateDbConnection&UserSchema

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("hello");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+require("./src/config/mongoose");
