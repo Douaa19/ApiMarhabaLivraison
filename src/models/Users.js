ab98bb4f416c498ad01e4749030e1288c594daaa
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-03-30 16:58:35 +0000

    createFunctionPreInUsersModelForHashingThePassword

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Role = new mongoose.Schema({
   name: {
@@ -35,6 +36,16 @@ const Users = new mongoose.Schema({
     maxlength: [20, "the password must be less than 20 characters"],
   },
   role: Role,
+});
+
+Users.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  bcrypt.hash(user.password, 10).then((hashedPassword) => {
+    user.password = hashedPassword;
+    next();
+  });
 });
 
 const User = mongoose.model("User", Users);
