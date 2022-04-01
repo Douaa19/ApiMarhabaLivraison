a6723225fe33d0404e83531b1495b7cca2b51f50
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-04-01 15:14:59 +0000

    getAllRefusedDeliveryGuys

--- src/controllers/DeliveryGuys.js
+++ src/controllers/DeliveryGuys.js
@@ -3,21 +3,38 @@ const { User } = require("../models");
 
 // Gat all delivery guys how were accepted
 const getAccepted = async (req, res) => {
-  User.where("role.status")
-    .equals("accepted")
-    .exec()
-    .then((result) => {
-      if (!result) {
-        res.status(404).json({ message: "No accepted delivery guy found" });
-      } else {
-        res.status(200).json({ result });
-      }
-    });
+  try {
+    User.where("role.status")
+      .equals("accepted")
+      .exec()
+      .then((result) => {
+        if (!result) {
+          res.status(404).json({ message: "No accepted delivery guy found" });
+        } else {
+          res.status(200).json({ result });
+        }
+      });
+  } catch (error) {
+    res.json(error.message);
+  }
 };
 
 // Gat all delivery guys how were refused
-const getRefused = (req, res) => {
-  console.log("Get all refused delivery guys");
+const getRefused = async (req, res) => {
+  try {
+    User.where("role.status")
+      .equals("refused")
+      .exec()
+      .then((result) => {
+        if (!result) {
+          res.status(404).json({ message: "No refused delivery guy found" });
+        } else {
+          res.status(200).json({ result });
+        }
+      });
+  } catch (error) {
+    res.json(error.message);
+  }
 };
 
 module.exports = {
