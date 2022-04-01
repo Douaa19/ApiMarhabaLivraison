593655863c561bc055be7d1ce82707c863732f1e
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-04-01 15:12:14 +0000

    getAllAcceptedDeliveryGuys

--- src/controllers/DeliveryGuys.js
+++ src/controllers/DeliveryGuys.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
-const { Users } = require("../models");
+const { User } = require("../models");
 
 // Gat all delivery guys how were accepted
-const getAccepted = (req, res) => {
-    console.log("Get all accepted delivery guys");
-}
+const getAccepted = async (req, res) => {
+  User.where("role.status")
+    .equals("accepted")
+    .exec()
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({ message: "No accepted delivery guy found" });
+      } else {
+        res.status(200).json({ result });
+      }
+    });
+};
 
 // Gat all delivery guys how were refused
 const getRefused = (req, res) => {
-    console.log("Get all refused delivery guys");
-}
+  console.log("Get all refused delivery guys");
+};
 
 module.exports = {
-    getAccepted,
-    getRefused,
-}
+  getAccepted,
+  getRefused,
+};
