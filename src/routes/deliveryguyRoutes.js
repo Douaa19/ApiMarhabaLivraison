8cb7a94cef49826ee6fd6487b404e4de59e294bd
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-04-05 02:30:25 +0000

    showOrdersStatus

--- src/routes/deliveryguyRoutes.js
+++ src/routes/deliveryguyRoutes.js
@@ -12,4 +12,7 @@ router.route("/newCommands").get(Commands.getNewCommands);
 // Get one command
 router.route("/command").get(Commands.getCommand).put(Commands.updateStatus);
 
+// Command status
+router.route("/status").get(Commands.statusCommand);
+
 module.exports = router;
